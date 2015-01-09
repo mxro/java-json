dafb25b22032b84e160ce6d6d35c91b92d529d94
mxro <mxro@nowhere.com>
2015-01-09 14:34:00 +1100

    Implementing first version of java-json project Autocommit Fri Jan 09 14:34:00 EST 2015

--- src/main/java/de/mxro/json/JSON.java
+++ src/main/java/de/mxro/json/JSON.java
@@ -13,10 +13,33 @@ public abstract class JSON {
         return new JSONImpl();
     }
 
+    /**
+     * <p>
+     * Add a new entry to this JSON.
+     * 
+     * 
+     * @param key
+     *            The key for the value.
+     * @param value
+     *            Value for the key. Can be a generic object, a string, or an
+     *            instance of {@link JSON}
+     * 
+     */
     public abstract void add(String key, Object value);
 
+    /**
+     * 
+     * @return The rendered string representation of this JSON document.
+     */
     public abstract String render();
 
+    /**
+     * 
+     * @param indentSize
+     *            Defines how many spaces are to be inserted at the beginning of
+     *            ever line.
+     * @return Th
+     */
     public abstract String render(int indentSize);
 
 }
