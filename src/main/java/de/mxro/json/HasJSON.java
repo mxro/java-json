8e47b7ab9729e6b98353c9cf2f0b6236acf40eea
mxro <mxro@nowhere.com>
2015-01-09 14:31:00 +1100

    Implementing first version of java-json project Autocommit Fri Jan 09 14:31:00 EST 2015

--- src/main/java/de/mxro/json/HasJSON.java
+++ src/main/java/de/mxro/json/HasJSON.java
@@ -1,5 +1,11 @@
 package de.mxro.json;
 
+/**
+ * Marks a class which can be rendered into JSON.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
 public interface HasJSON {
     public JSON getJSON();
 }
