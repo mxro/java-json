2efff9f858b7d1a742e8b88f82ef67f61db5b628
mxro <mxro@nowhere.com>
2015-01-14 13:20:13 +1100

    Implemeting first version of async-api Autocommit Wed Jan 14 13:20:13 EST 2015

--- src/main/java/de/mxro/json/internal/JSONArrayImpl.java
+++ src/main/java/de/mxro/json/internal/JSONArrayImpl.java
@@ -1,5 +1,42 @@
 package de.mxro.json.internal;
 
-public class JSONArrayImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import de.mxro.json.JSONArray;
+
+public class JSONArrayImpl implements JSONArray {
+
+    private final List<Object> items;
+
+    @Override
+    public JSONArray push(final Object value) {
+        items.add(value);
+        return this;
+    }
+
+    @Override
+    public String render() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String render(final int indentSize) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public JSONArrayImpl() {
+        super();
+        this.items = new ArrayList<Object>();
+
+    }
 
 }
