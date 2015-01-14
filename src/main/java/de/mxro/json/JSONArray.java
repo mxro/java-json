aa8fc6b1d4fcec0e0454b9b1e6ca376c68acd34b
mxro <mxro@nowhere.com>
2015-01-14 13:21:13 +1100

    Implemeting first version of async-api Autocommit Wed Jan 14 13:21:13 EST 2015

--- src/main/java/de/mxro/json/JSONArray.java
+++ src/main/java/de/mxro/json/JSONArray.java
@@ -4,4 +4,24 @@ public interface JSONArray {
 
     public JSONArray push(Object value);
 
+    /**
+     * 
+     * @return The rendered string representation of this JSON document.
+     */
+    public abstract String render();
+
+    /**
+     * 
+     * @param indentSize
+     *            Defines how many spaces are to be inserted at the beginning of
+     *            ever line.
+     * @return The rendered string representation of this JSON document.
+     */
+    public abstract String render(int indentSize);
+
+    /**
+     * <p>
+     * Prints this document to standard out.
+     */
+    public abstract void print();
 }
